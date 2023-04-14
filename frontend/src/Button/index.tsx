eb06873abf3547e7c9bb35b2ac1fefc91b89e4e3
Kupriienko <Kupriienko>
2023-04-14 19:35:25 +0300

    fixed code

--- frontend/src/Button/index.tsx
+++ frontend/src/Button/index.tsx
@@ -2,13 +2,13 @@ import Arrow from './arrow.svg';
 import './styles.css';
 
 interface Props {
-    onClick: () => void;
-    disabled: boolean;
+    onClick?: () => void;
+    disabled?: boolean;
 }
 
 function Button({onClick, disabled}: Props) {
     return (
-        <button  className={disabled ? 'button-unavailable': ''} disabled={disabled} onClick={onClick} >
+        <button className={disabled ? 'button-disabled': ''} disabled={disabled} onClick={onClick} >
             <img className="button_img" src={Arrow} alt="Go" />
         </button>
     );
