c0c4bab17006c9eb11c96025dfdc3253ff9fe325
Kupriienko <Kupriienko>
2023-04-30 11:03:50 +0300

    contorlled input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import MenuBar from './MenuBar';
 import Riddles from './Riddles';
 import Button from './Button';
@@ -5,6 +6,7 @@ import Input from './Input';
 import './App.css';
 
 function App() {
+    const [answer, setAnswer] = useState('');
 
     async function verify() {
         console.log('clicked');
@@ -18,7 +20,7 @@ function App() {
           {'riddle': 'I follow you all the time and copy your every move, but you can’t touch me or catch me. ' +
                   'What am I?', 'id': 2, 'creationDate': new Date(2023, 11, 14)}]}/>
           <Button onClick = {verify} disabled = {false} />
-          <Input inputValue={"shadow"} onChange = {(value) => console.log(value)} />
+          <Input value={answer} onChange = {(newAnswer) => setAnswer(newAnswer)} />
       </div>
   );
 }
--- frontend/src/Input/index.tsx
+++ frontend/src/Input/index.tsx
@@ -1,22 +1,15 @@
-import { useState } from "react";
 import './styles.css';
 
 export type Props = {
-    inputValue?: string;
+    value?: string;
     onChange?: (value: string) => void;
 };
 
-function Input({ inputValue, onChange }: Props) {
-    const [value, setValue] = useState(inputValue || "");
-
+function Input({ value, onChange }: Props) {
     return (
         <input
-            className="input" type="text" name="answer" value={value} onChange={(event) => {
-                if (onChange) {
-                    onChange(event.target.value);
-                    setValue(event.target.value);
-                }
-            }}
+            className="input" type="text" name="answer" value={value}
+            onChange={(event) => {if (onChange) {onChange(event.target.value)}}}
         />
     );
 }
