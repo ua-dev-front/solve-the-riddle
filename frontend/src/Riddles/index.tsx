7be171b5fdb36b93f4bc4c85ee3406211a401247
Kupriienko <Kupriienko>
2023-07-07 12:44:22 +0300

    added preloader, fixed code

--- frontend/src/Riddles/index.tsx
+++ frontend/src/Riddles/index.tsx
@@ -1,25 +1,34 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Riddle from '../Riddle';
+import Preloader from './Preloader.svg';
 import './styles.css';
 
 const localURL = 'http://127.0.0.1:5000/';
 
 function Riddles() {
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function getData() {
             const response = await fetch(localURL);
             let actualData = await response.json();
             setData(actualData['riddles']);
+            setLoading(false);
         }
         getData();
     }, []);
 
     return (
         <div className="riddles">
-            {data.map((riddle) => (<Riddle riddle={riddle['riddle']} id={riddle['id']} key={riddle['id']}
-                                           creationDate={riddle['creationDate']}/>))}
+            {loading ? (
+                <img src={Preloader} alt={"Loading..."} className="riddles_preloader" />
+            ) : (
+                data.map((riddle) => (
+                    <Riddle riddle={riddle['riddle']} id={riddle['id']} key={riddle['id']}
+                            creationDate={riddle['creationDate']}/>
+                ))
+            )}
         </div>
     );
 }
