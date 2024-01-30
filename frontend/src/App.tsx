ac7e3aa74311805ac1bd9e0f3468e43ec3a9994c
Kupriienko <Kupriienko>
2024-01-30 19:47:21 +0200

    fixed code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MenuBar from './MenuBar';
 import Riddles from './Riddles';
 import SignInButton from './SignInButton';
@@ -7,6 +7,15 @@ import './App.css';
 
 function App() {
     const [isSignInFormVisible, setIsSignInFormVisible] = useState(false);
+
+    useEffect(() => {
+        function handleKeyPress(event: KeyboardEvent) {
+            if (event.keyCode === 27 && isSignInFormVisible) {
+                setIsSignInFormVisible(false);
+            }
+        }
+        document.addEventListener('keydown', handleKeyPress);
+    }, [isSignInFormVisible]);
 
     function toggleForm() {
         setIsSignInFormVisible(!isSignInFormVisible);
